7fb38d7f4b6be4c9336ece5f8e57afc19743ba80
Erthium <ertyum35@gmail.com>
2024-05-05 23:10:28 +0300

    feat: new loading layer to be used everywhere

---
+++ components/star-wars-editor/loading-layer.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+const LoadingLayer: React.FC = () => {
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black opacity-50"></div>
+        <div className="absolute bottom-1/2 right-1/2 items-center justify-center">
+          <PacmanLoader color="#FBBF24"/>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoadingLayer;
