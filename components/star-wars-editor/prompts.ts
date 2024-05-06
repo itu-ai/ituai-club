85e290f4159c4cc2b8526671a3b2ab4d878206fd
Erthium <ertyum35@gmail.com>
2024-05-06 09:08:33 +0300

    feat: change predefined prompt configs and give type to style preset

--- components/star-wars-editor/prompts.ts
+++ components/star-wars-editor/prompts.ts
@@ -2,44 +2,64 @@ export interface PromptData {
   title: string,
   prompt: string,
   strength: number, // 0-1 float
-  style_preset: string | null,
+  style_preset: "enhance" | "anime" | "photographic" | "digital-art" | "comic-book" | "fantasy-art" | "line-art" | "analog-film" | "neon-punk" | "isometric" | "low-poly" | "origami" | "modeling-compound" | "cinematic" | "3d-model" | "pixel-art" | "tile-texture" | null,
   beforeImage?: string,
   afterImage?: string,
   cfg_scale: number // 0-35 int
 }
 
 /* -- Style Presets --
-enhance
-anime
-photographic
-digital-art
-comic-book
-fantasy-art
-line-art
-analog-film
-neon-punk
-isometric
-low-poly
-origami
-modeling-compound
-cinematic
-3d-model
-pixel-art
-tile-texture
+"enhance"
+"anime"
+"photographic"
+"digital-art"
+"comic-book"
+"fantasy-art"
+"line-art"
+"analog-film"
+"neon-punk"
+"isometric"
+"low-poly"
+"origami"
+"modeling-compound"
+"cinematic"
+"3d-model"
+"pixel-art"
+"tile-texture"
 */
+
+export const PresetList = [
+  "enhance",
+  "anime",
+  "photographic",
+  "digital-art",
+  "comic-book",
+  "fantasy-art",
+  "line-art",
+  "analog-film",
+  "neon-punk",
+  "isometric",
+  "low-poly",
+  "origami",
+  "modeling-compound",
+  "cinematic",
+  "3d-model",
+  "pixel-art",
+  "tile-texture",
+]
 
 const jediPrompt: PromptData = {
   title: "Jedi Master",
-  prompt: "A jedi with a lightsaber",
-  strength: 0.3,
+  prompt: "In Star Wars universe, an epic scene of a brave Jedi Master",
+  strength: 0.5,
   style_preset: "cinematic",
   cfg_scale: 7
 }
 
 const sithLordPrompt: PromptData = {
   title: "Sith Lord",
-  prompt: "A sith lord with a double red lightsaber",
-  strength: 0.3,
+  prompt: "In Star Wars universe, an epic scene of a rageful Sith Lord",
+  strength: 0.5,
   style_preset: "cinematic",
   cfg_scale: 7
 }
