255d4628da8488a04d471975a7811ebaea8334ec
Erthium <ertyum35@gmail.com>
2024-07-16 06:57:58 +0300

    feat: create a simple landing page to redirect editor pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <h1 className="z-10 my-10 text-3xl text-transparent duration-1000 bg-black cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-6xl whitespace-nowrap bg-clip-text ">
-        ITU AI Projects
+    <div className="flex flex-col items-center justify-between min-h-screen overflow-x-hidden">
+      <h1 className="text-center font-semibold text-3xl mt-12 text-zinc-200">
+        This website is currently hosting projects of ITU Artificial Intelligence Club
       </h1>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="grid grid-cols-2 gap-8">
+
+        <div className="max-w-[24rem]">
+          <Link
+            href="/editor/summit"
+          >
+            <img
+              src="/editor_summit.png"
+              alt="Editor Summit"
+              className="border-4 mb-4 border-zinc-800 hover:border-zinc-900 duration-400 transition-colors"
+            />
+          </Link>
+          <p className="text-center text-base font-normal text-zinc-300">
+            Image Editor made for AI Summit'24
+          </p>
+        </div>
+
+        <div className="max-w-[24rem]">
+          <Link
+            href="/editor/starwars"
+          >
+            <img
+              src="/editor_starwars.png"
+              alt="Editor Star Wars"
+              className="border-4 mb-4 border-zinc-800 hover:border-zinc-900 duration-400 transition-colors"
+            />
+          </Link>
+          <p className="text-center text-base font-normal text-zinc-300">
+            Image Editor made for Star Wars Movie Night
+          </p>
+        </div>
+
+      </div>
+      <footer className=" pb-6">
+        <p className="text-center text-zinc-500">
+          © 2024 ITU Artificial Intelligence Club. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
-
 }
