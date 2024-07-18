37459b37d465ad29e8f45cde2103dc298f571e69
Erthium <ertyum35@gmail.com>
2024-07-18 12:01:10 +0300

    fix: the position of result text

--- components/turing/result.tsx
+++ components/turing/result.tsx
@@ -9,9 +9,9 @@ const TuringResult: React.FC<Props> = ({ text }) => {
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-        <div className="fixed inset-0 bg-red-500 opacity-50"></div>
-        <div className="absolute bottom-1/2 right-1/2 items-center justify-center">
-          <div className="px-12 py-6 bg-zinc-800">
+        <div className="fixed inset-0 bg-zinc-800 opacity-50 z-2"></div>
+        <div className="flex items-center justify-center z-10">
+          <div className="px-6 py-4 bg-zinc-800 flex items-center justify-center rounded-xl">
             <p className="text-center font-bold text-xl text-zinc-200">
               {text}
             </p>
