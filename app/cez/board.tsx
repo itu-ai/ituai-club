5b6b72bc3c15add0624f14d27ff6d1b75c5f1611
Erthium <ertyum35@gmail.com>
2024-07-27 00:42:45 +0300

    feat: show move history and whose turn information

--- app/cez/board.tsx
+++ app/cez/board.tsx
@@ -8,21 +8,32 @@ interface Props {
   board_size?: number;
 }
 
+interface GameStats {
+  isGameOver: boolean;
+  winnerSide: "white" | "black" | "draw" | "none";
+  isWhitesTurn: boolean;
+  moveHistory: Move[];
+}
+
 export const CezBoard: React.FC<Props> = ({ board_size = 720 }) => {
-  // canvas hooks
+  // canvas variabels
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [grid, setGrid] = useState<Grid>(new Grid());
-  
-  // mouse event hooks
   let selectedPiece: Tile | null = null;
-
-  // contants
   const tile_size = board_size / 8;
 
   // theme/colors
   const light_tile_color = '#f0d9b5';
   const dark_tile_color = '#b58863';
-  
+
+  // game control variables
+  const [gameStats, setGameStats] = useState<GameStats>({
+    isGameOver: false,
+    winnerSide: 'none',
+    isWhitesTurn: true,
+    moveHistory: []
+  });
+
   const drawBoard = () => {
     const canvas = canvasRef.current;
     const ctx = canvas?.getContext('2d');
@@ -63,10 +74,31 @@ export const CezBoard: React.FC<Props> = ({ board_size = 720 }) => {
       });
     }
   }
-  
+
   useEffect(() => {
     drawBoard();
   });
+
+  const handlePieceMove = (move: Move) => {
+    grid.movePiece(move);
+    selectedPiece = null;
+    if (grid.isGameOver) {
+      setGameStats({
+        isGameOver: true,
+        winnerSide: grid.winnerSide,
+        isWhitesTurn: grid.isWhitesTurn,
+        moveHistory: grid.moveHistory,
+      });
+    }
+    else {
+      setGameStats({
+        isGameOver: false,
+        winnerSide: 'none',
+        isWhitesTurn: grid.isWhitesTurn,
+        moveHistory: grid.moveHistory,
+      });
+    }
+  }
 
   const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
     // get the square/tile the mouse is in
@@ -76,9 +108,7 @@ export const CezBoard: React.FC<Props> = ({ board_size = 720 }) => {
     if (selectedPiece) {
       const move = grid.legalMoves.find((move: Move) => selectedPiece !== null && move.to.x === x && move.to.y === y && move.from.x === selectedPiece.x && move.from.y === selectedPiece.y);
       if (move) {
-        grid.movePiece(move);
-        selectedPiece = null;
-        
+        handlePieceMove(move);
       }
       else {
         selectedPiece = null;
@@ -98,13 +128,64 @@ export const CezBoard: React.FC<Props> = ({ board_size = 720 }) => {
   }
 
   return (
-    <canvas 
-      id="cez-board"
-      width={board_size}
-      height={board_size}
-      ref={canvasRef}
-      onMouseDown={handleMouseDown}
-    />
+    <div className="flex flex-col lg:flex-row items-start justify-center gap-4">
+      {/* Game Visuals/Canvas */}
+      <canvas 
+        id="cez-board"
+        width={board_size}
+        height={board_size}
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+      />
+      {/* Game Controls */}
+      <div className="flex flex-col w-full h-auto lg:w-[24rem] lg:h-full items-start justify-start gap-4">
+        {!gameStats.isGameOver &&
+          <>
+            <p className="text-start font-medium text-zinc-300">
+              {`Turn: ${grid.isWhitesTurn ? 'White' : 'Black'}`}
+            </p>
+          </>
+        }
+        {gameStats.isGameOver && 
+          <>
+            <p className="text-start font-medium text-zinc-300">
+              {`Game Over. Winner: ${gameStats.winnerSide}`}
+            </p>
+          </>
+        }
+        {/* Move History */}
+        <div className="grid grid-cols-2 w-full lg:w-auto h-auto lg:h-[24rem] items-start justify-start gap-2 overflow-y-auto">
+          {/* White moves */}
+          <div className="flex flex-col items-start justify-start gap-2">
+            <p className="text-start font-medium text-zinc-300">White Moves</p>
+            {gameStats.moveHistory.map((move: Move, index: number) => {
+              if (index % 2 === 0) {
+                return (
+                  <p className="text-start font-light text-zinc-300" key={index}>
+                    {`${index + 1}. ${move.from.x},${move.from.y} to ${move.to.x},${move.to.y}`}
+                  </p>
+                );
+              }
+              return null;
+            })}
+          </div>
+            {/* Black moves */}
+          <div className="flex flex-col items-start justify-start gap-2">
+            <p className="text-start font-medium text-zinc-300">Black Moves</p>
+            {gameStats.moveHistory.map((move: Move, index: number) => {
+              if (index % 2 === 1) {
+                return (
+                  <p className="text-start font-light text-zinc-300" key={index}>
+                    {`${index + 1}. ${move.from.x},${move.from.y} to ${move.to.x},${move.to.y}`}
+                  </p>
+                );
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
