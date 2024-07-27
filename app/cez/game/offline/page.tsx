c5e006f4dd5222e23ad7eff9fe58e6634ddda67c
Erthium <ertyum35@gmail.com>
2024-07-27 22:39:06 +0300

    feat: simple adjusments for online and ai gameplay preparation

---
+++ app/cez/game/offline/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { CezBoard } from "@/components/cez/board";
+
+export default function CezPage() {
+  const [board_size, setBoardSize] = useState<number>(720);
+  useEffect (() => {
+    // get minimum of window width and height
+    setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
+    window.addEventListener('resize', () => {
+      setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
+    });
+    return () => {
+      window.removeEventListener('resize', () => {
+        setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center text-white mt-4 lg:mt-8">
+        Cez
+      </h1>
+      <div className="py-4 mt-4">
+        <CezBoard board_size={board_size} />
+      </div>
+    </div>
+  );
+}
--- app/cez/page.tsx
+++ app/cez/page.tsx
@@ -1,31 +1,38 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { CezBoard } from "./board";
+import Link from "next/link";
 
 export default function CezPage() {
-  const [board_size, setBoardSize] = useState<number>(720);
-  useEffect (() => {
-    // get minimum of window width and height
-    setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
-    window.addEventListener('resize', () => {
-      setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
-    });
-    return () => {
-      window.removeEventListener('resize', () => {
-        setBoardSize(Math.min(window.innerWidth, window.innerHeight) * 0.8);
-      });
-    }
-  }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-center text-white mt-4 lg:mt-8">
-        Cez
+    <div className="flex flex-col items-center justify-center px-4 lg:px-0">
+      <h1 className="text-4xl font-bold text-center text-zinc-200 mt-6 lg:mt-8">
+        Cez - Chess Variation
       </h1>
-      <div className="py-4 mt-4">
-        <CezBoard board_size={board_size} />
+      <p className="text-xl font-medium text-center text-zinc-200 mt-6 lg:mt-8">
+        Cez is a chess variation, with the same board but different pieces and rules.
+      </p>
+      <div className="flex flex-col lg:flex-row gap-4 mt-6 lg:mt-8">
+        <Link
+          href="/cez/game/offline"
+        >
+          <p className="text-xl font-bold text-center text-zinc-300 px-4 py-2 border-2 border-zinc-700 rounded-xl hover:border-zinc-800 hover:text-zinc-400">
+            Try Offline Beta
+          </p>
+        </Link>
+        <button disabled={true} className="group">
+          <p className="text-xl font-bold text-center text-zinc-300 px-4 py-2 border-2 border-zinc-700 rounded-xl hover:border-zinc-800 hover:text-zinc-400 group-disabled:border-zinc-900 group-disabled:text-zinc-500">
+            Against AI (Soon)
+          </p>
+        </button>
+        <button disabled={true}  className="group">
+          <p className="text-xl font-bold text-center text-zinc-300 px-4 py-2 border-2 border-zinc-700 rounded-xl hover:border-zinc-800 hover:text-zinc-400 group-disabled:border-zinc-900 group-disabled:text-zinc-500">
+            Online (Soon)
+          </p>
+        </button>
       </div>
+      <h2 className="text-2xl font-bold text-center text-zinc-200 mt-6 lg:mt-8">
+        Rules
+      </h2>
+
     </div>
   );
 }
