ef2673bf558e22986feae51d278eb94b4a98cf02
Erthium <ertyum35@gmail.com>
2024-07-31 21:27:23 +0300

    feat: put everything together and push for v1.0

--- app/turing/reverse/page.tsx
+++ app/turing/reverse/page.tsx
@@ -1,9 +1,50 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+
+import { TuringChat } from "@/components/reverse-turing/turing-chat";
+import CharacterSelection from "@/components/reverse-turing/character-selection";
+import TuringTestResult from "@/components/reverse-turing/turing-test-result";
+
+import { Character } from "@/interfaces/turing/character";
+import { AgentGuess } from "@/interfaces/turing/agent-guess";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 lg:px-0">
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(null);
+  const [agentGuesses, setAgentGuesses] = useState<AgentGuess[] | null>(null);
 
+  const handleOnCharacterSelect = (character: Character) => {
+    setSelectedCharacter(character);
+  }
+
+  const handleOnTestFinished = (agent_guesses: AgentGuess[]) => {
+    setAgentGuesses(agent_guesses);
+  }
+
+  const handleRestart = () => {
+    setSelectedCharacter(null);
+    setAgentGuesses(null);
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen px-4 lg:px-0">
+      {selectedCharacter === null &&
+        <CharacterSelection
+          onCharacterSelect={handleOnCharacterSelect}
+        />
+      }
+      {selectedCharacter && !agentGuesses &&
+        <TuringChat
+          human_character={selectedCharacter}
+          onTestFinished={handleOnTestFinished}
+        />
+      }
+      {agentGuesses &&
+        <TuringTestResult
+          agent_guesses={agentGuesses}
+          onRestart={handleRestart}
+        />
+      }
     </div>
   );
 }
