1d8a4f19d640cc6f099a7b35dd5638b1a2926206
Erthium <ertyum35@gmail.com>
2024-07-31 21:25:56 +0300

    feat: arrange necessary interfaces for reverse turing test

---
+++ interfaces/turing/agent-guess.ts
@@ -0,0 +1,6 @@
+import { Agent } from './agent';
+
+export interface AgentGuess {
+  agent: Agent;
+  guess: string;
+}
--- interfaces/turing/message.ts
+++ interfaces/turing/message.ts
@@ -1,7 +1,6 @@
 import { Agent } from "./agent";
 
 export interface Message {
-  id?: number;
   agent: Agent;
   content: string;
   createdAt: Date;
