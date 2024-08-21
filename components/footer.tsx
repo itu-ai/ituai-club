445f61a95e26367674cd74a378f3402a0fb0d0df
Erthium <ertyum35@gmail.com>
2024-08-21 04:36:26 +0300

    feat: add text colow prop to footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,12 @@
 interface Props {
   className?: string;
+  textColor?: string;
 }
 
-const Footer: React.FC<Props> = ({ className }) => {
+const Footer: React.FC<Props> = ({ className, textColor = "zinc-500" }) => {
   return (
     <footer className={`flex w-full items-center justify-center pb-6 ${className}`}>
-      <p className="text-center text-zinc-500 text-xs lg:text-base">
+      <p className={`text-center text-${textColor} text-xs lg:text-base`}>
         © 2024 ITU Artificial Intelligence Club. All rights reserved.
       </p>
     </footer>
