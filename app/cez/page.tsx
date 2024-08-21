c3d1d8250d582b8948fdfd95090fc6a48bd39300
Erthium <ertyum35@gmail.com>
2024-08-21 04:36:13 +0300

    feat: add gifs to the rules and make the rules section of cez mobile compatible

--- app/cez/page.tsx
+++ app/cez/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Footer from "@/components/footer";
 
 export default function CezPage() {
 
@@ -52,41 +53,87 @@ export default function CezPage() {
           </button>
         </div>
 
+        {/* Rules */}
+        <div className="flex flex-col max-w-6xl pb-24 mt-6 lg:mt-12">
+          <h2 className="text-2xl lg:text-3xl font-bold text-center text-zinc-200 mt-6 lg:mt-6">
             Rules
           </h2>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+          {/* Board & Initial Position */}
+          <p className="text-sm lg:text-base font-normal text-start lg:text-center text-zinc-200 mt-6 lg:mt-6">
             The game is played on a 8 by 8 square board just like a chess board. 
           </p>
-          <h3 className="text-xl font-bold text-start text-zinc-200 mt-6 lg:mt-6">
-            Pieces
+
+          <h3 className="text-lg lg:text-xl font-bold text-center text-zinc-200 mt-6 lg:mt-12 mb-4">
+            Pieces - Pawns & Knights
           </h3>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+          <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6 lg:-mb-6">
             There are two types of pieces in Cez.
           </p>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
-            Pawns move one square either horizontally or vertically (not diagonally). However; if the square that a pawn is going to move is occupied by an opponent pawn, the pawn can not move to that square. Instead, it can take it by jumping over it and moving one more square (2 squares in total). The square that the pawn is going to land must not be occupied by either a friend or opponent piece. These rules make the game look very like checkers, but unlike in checkers the pieces can not perform multiple captures in one turn.
-          </p>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
-            Knigths move two squares either horizontally or vertically just like pawns, and jumps over the piece right in front of them like they do in chess. To capture a piece, they jump over the opponent piece and move a total of 3 squares.
-          </p>
-          <h3 className="text-xl font-bold text-start text-zinc-200 mt-6 lg:mt-6">
+          {/* Pawns */}
+          <div className="flex flex-col lg:flex-row items-center justify-between gap-y-12 lg:gap-x-16 mb-12 lg:mb-0">
+            <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+              Pawns move one square either horizontally or vertically (not diagonally). However; if the square that a pawn is going to move is occupied by an opponent pawn, the pawn can not move to that square. Instead, it can take it by jumping over it and moving one more square (2 squares in total). The square that the pawn is going to land must not be occupied by either a friend or opponent piece. These rules make the game look very like checkers, but unlike in checkers the pieces can not perform multiple captures in one turn.
+            </p>
+            <img
+              src="/cez/pawn_movement.gif"
+              alt="Cez Pawn"
+              className="w-64 h-64"
+            />
+          </div>
+          {/* Knights */}
+          <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-y-12 lg:gap-x-16 mb-12 lg:mb-0">
+            <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+              Knigths move two squares either horizontally or vertically just like pawns, and jumps over the piece right in front of them like they do in chess. To capture a piece, they jump over the opponent piece and move a total of 3 squares.
+            </p>
+            <img
+              src="/cez/knight_movement.gif"
+              alt="Cez Pawn"
+              className="w-64 h-64"
+            />
+          </div>
+
+          <h3 className="text-lg lg:text-xl font-bold text-center text-zinc-200 mt-6 lg:mt-12 mb-4">
             Progressing through the game
           </h3>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
-            In the game; if any piece can be taken, a capturing move must be performed. If there are multiple captures available, any capturing move can be selected.
-          </p>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
-            The &quot;centered islands&quot; rule states that a player can win the game by creating &quot;island&quot;s which are made of pieces that are direct horizontal or vertical neighbors to other pieces in that island. To win, the player must create these  &quot;island&quot;s on the center pieces so that every &quot;island&quot; has at least a piece in the center squares (the d4. d5, e4 and the e5 squares).
-          </p>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
-            Another way to win the game is to capture all of the opponent pieces.
-          </p>
-          <p className="text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+          {/* Capturing */}
+          <div className="flex flex-col lg:flex-row items-center justify-between gap-y-12 lg:gap-x-16 mb-12 lg:mb-0">
+            <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+              In the game; if any piece can be taken, a capturing move must be performed. If there are multiple captures available, any capturing move can be selected.
+            </p>
+            <img
+              src="/cez/capturing.gif"
+              alt="Cez Pawn"
+              className="w-64 h-64"
+            />
+          </div>
+          {/* Winning */}
+          <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-y-12 lg:gap-x-16 mb-12 lg:mb-0">
+            <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+              The &quot;centered islands&quot; rule states that a player can win the game by creating &quot;island&quot;s which are made of pieces that are direct horizontal or vertical neighbors to other pieces in that island. To win, the player must create these  &quot;island&quot;s on the center pieces so that every &quot;island&quot; has at least a piece in the center squares (the d4. d5, e4 and the e5 squares).
+            </p>
+            <img
+              src="/cez/winning.gif"
+              alt="Cez Pawn"
+              className="w-64 h-64"
+            />
+          </div>
+          {/* No Piece Rule */}
+          <div className="flex flex-col lg:flex-row items-center justify-between gap-y-12 lg:gap-x-16 mb-12 lg:mb-0">
+            <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
+              Another way to win the game is to capture all of the opponent pieces. If the opponent has no pieces left, the player wins the game.
+            </p>
+            <img
+              src="/cez/no_piece_rule.gif"
+              alt="Cez Pawn"
+              className="w-64 h-64"
+            />
+          </div>
+          <p className="text-sm lg:text-base font-normal text-start text-zinc-200 mt-6 lg:mt-6">
             If both players meet the conditions of the &quot;interconnected islands&quot; rule at the same time, the game ends in a draw. If any position is repeated 3 times in the game, the game ends in a draw.
           </p>
         </div>
       </div>
-
+      <Footer/>
     </div>
   );
 }
